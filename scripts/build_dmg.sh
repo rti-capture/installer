8e404bde11e3b4db5c62db14057902657571f6a5
Don Cruickshank <dgc@ecs.soton.ac.uk>
2015-10-04 12:08:33 +0100

    Changed working directory in installation script.

--- scripts/build_dmg.sh
+++ scripts/build_dmg.sh
@@ -9,8 +9,8 @@ rm -rf "$APP"
 cp -r "$SOURCE" "$TARGET"
 
 # Install the installation script
-cp script.json "$APP/Contents/Resources/Java"
+cp script.json "$APP/Contents"
 
-cp "target/rti-vips-installer-1.0-SNAPSHOT.jar" "target/RTI-VIPS Installer.app/Contents/Java//org/glacsweb/rti/rti-vips-installer/1.0-SNAPSHOT/rti-vips-installer-1.0-SNAPSHOT.jar"
+cp "target/rti-vips-installer-1.0-SNAPSHOT.jar" "target/RTI-VIPS Installer.app/Contents/Java/org/glacsweb/rti/rti-vips-installer/1.0-SNAPSHOT/rti-vips-installer-1.0-SNAPSHOT.jar"
 
 hdiutil create -size 500m -srcfolder "target/RTI-VIPS Installer.app" -o "target/RTI-VIPS Installer.dmg"
